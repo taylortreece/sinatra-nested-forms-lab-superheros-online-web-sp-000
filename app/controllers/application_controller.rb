fd30964b51de4b0ca7bb8af46014fc66a807d9f2
Taylor Treece <taylor.treece@protonmail.com>
2021-03-09 17:48:28 -0600

    commit tree 27f33fcf4b7e92c5eb2c9e3fb2e1cd892b7fa8e1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :index
+    end
+
+    post '/teams' do
+        @params=params
+        erb :team
+    end
+
 
 end
